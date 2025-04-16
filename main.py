abcb6d134cdc1a06196b1854652a88d6636c09f0
Dawid Marszałek <dawidmarszaleq@gmail.com>
2025-04-16 17:56:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, url_for
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FLASK_KEY = os.environ.get("SECRET_KEY")
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=False, port=5000)
